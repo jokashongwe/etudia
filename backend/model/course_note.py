51f8dd6dfce9180d52dfc39492ab8303376a0cda
Jonathan Kashongwe <jokashongwe@gmail.com>
2024-03-14 23:23:20 +0100

    update model

--- backend/model/course_note.py
+++ backend/model/course_note.py
@@ -1,7 +1,7 @@
 from pydantic import Field
 from backend.classes.object_id import PydanticObjectId
 from typing import Optional
-from backend.model import Model
+from ..model import Model
 from datetime import datetime
 
 class CourseNote(Model):
--- backend/model/user_note.py
+++ backend/model/user_note.py
@@ -2,7 +2,7 @@
 from pydantic import Field
 from backend.classes.object_id import PydanticObjectId
 from typing import Optional
-from backend.model import Model
+from ..model import Model
 from datetime import datetime
 
 class UserNote(Model):
