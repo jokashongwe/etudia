301274530527786d7ba34d9cad6e04eb7b9b58bf
Jonathan Kashongwe <jokashongwe@gmail.com>
2024-03-14 23:53:07 +0100

    ggg

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+   name='Etudia',
+   author='Jonathan Kashongwe',
+   description='Etudia AI Bot',
+   version='0.1',
+   packages=['etudia'],
+   install_requires=['wheel', 'bar', 'greek'],
+   license='MIT',
+   author_email='jokashongwe@gmail.com',
+   long_description=open('README.txt').read(),
+)
