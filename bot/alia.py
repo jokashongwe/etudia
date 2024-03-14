b32855e5bcdb0518d0032331e71758a6b82bf384
Jonathan Kashongwe <jokashongwe@gmail.com>
2024-03-14 23:10:20 +0100

    suspend storage

--- bot/alia.py
+++ bot/alia.py
@@ -42,18 +42,23 @@ def find(query: str, promotion: str, course: str = "", subject: str= "", source:
     course = slugify(course) if course else ""
     source = slugify(source) if source else ""
     subject = slugify(subject) if subject else ""
-    storage_path = os.path.join(PERSIST_DIR, source, subject, promotion, course)
-    if not os.path.exists(storage_path):
-        # load the documents and create the index
-        doc_query = {"promotion": promotion, "course": course} if course else {"promotion": promotion}
-        documents = [doc for doc in get_documents(query=doc_query)]
-        index = VectorStoreIndex.from_documents(documents)
-        # store it for later
-        index.storage_context.persist(persist_dir=storage_path)
-    else:
-        # load the existing index
-        storage_context = StorageContext.from_defaults(persist_dir=storage_path)
-        index = load_index_from_storage(storage_context)
+    doc_query = {"promotion": promotion, "course": course} if course else {"promotion": promotion}
+    documents = [doc for doc in get_documents(query=doc_query)]
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    # index.storage_context.persist(persist_dir=storage_path)
+    # storage_path = os.path.join(PERSIST_DIR, source, subject, promotion, course) 
+    # if not os.path.exists(storage_path):
+    #     # load the documents and create the index
+    #     doc_query = {"promotion": promotion, "course": course} if course else {"promotion": promotion}
+    #     documents = [doc for doc in get_documents(query=doc_query)]
+    #     index = VectorStoreIndex.from_documents(documents)
+    #     # store it for later
+    #     index.storage_context.persist(persist_dir=storage_path)
+    # else:
+    #     # load the existing index
+    #     storage_context = StorageContext.from_defaults(persist_dir=storage_path)
+    #     index = load_index_from_storage(storage_context)
         
     query_engine = index.as_query_engine()
     return query_engine.query(query)
