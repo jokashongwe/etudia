7fdd16749208fb841fd0d2635df603d3706fcb9d
Jonathan Kashongwe <jokashongwe@gmail.com>
2024-03-19 18:53:17 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
    name='Etudia',
    author='Jonathan Kashongwe',
    description='Etudia AI Bot',
-   version='0.1.2',
+   version='0.1.3',
    packages=['backend', 'bot', 'backend.model', 'backend.classes'],
    install_requires=['wheel', 'bar', 'greek'],
    license='MIT',
